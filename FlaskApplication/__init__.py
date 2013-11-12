6ec74224aaa0c464f0092049fa892d110c8a4c7a
Bill Wilder <codingoutloud@gmail.com>
2013-11-12 11:39:19 -0500

    somewhat

--- FlaskApplication/__init__.py
+++ FlaskApplication/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    text = { 'content': 'Welcome to this respectable looking flask application !' } 
+    text = { 'content': 'Welcome to this somewhat respectable looking flask application !' } 
     return render_template("home.html",
         title = 'Home away from home',
         text = text)
