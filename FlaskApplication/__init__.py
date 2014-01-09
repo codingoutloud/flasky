3e55f8470ce0cde4d5a9aa228eb487396c59d2aa
Bill Wilder <codingoutloud@gmail.com>
2014-01-09 17:59:22 -0500

    restore (corrected) print

--- FlaskApplication/__init__.py
+++ FlaskApplication/__init__.py
@@ -1,17 +1,23 @@
 from flask import Flask
 from datetime import *
+from os import *
 from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    print('home page visit at %s UTC [currently running on COMPUTERNAME = %s]' % 
+            (datetime.utcnow(), getenv('COMPUTERNAME', '<i>unknown</i>')))
     text = { 'content': 'Welcome to this lovely looking flask application !' } 
-    return render_template("home.html",
+    return render_template('home.html',
         title = 'Flasky Home',
         text = text)
+
 
 
 app.debug = True
 if __name__ == "__main__":
     app.run()
+
+
