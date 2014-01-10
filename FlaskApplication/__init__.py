bab3486f9f9dc6d596f3a791e54ef3b98ad67746
Bill Wilder <codingoutloud@gmail.com>
2014-01-09 19:14:03 -0500

    better researched logging

--- FlaskApplication/__init__.py
+++ FlaskApplication/__init__.py
@@ -1,23 +1,22 @@
 from datetime import *
 from os import *
-from logging import *
 from flask import Flask
 from flask import render_template
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(filename='flasky.log',level=logging.DEBUG)
+logging.debug('LOG FILE ENABLED as file %s' % log_file)
+
 @app.route('/')
 def home():
-    Logger.warning('helllllloo')
-    Logger.error('danger Will Robinson')
-    print('home page visit at %s UTC [currently running on COMPUTERNAME = %s]' % 
-            (datetime.utcnow(), getenv('COMPUTERNAME', '<i>unknown</i>')))
+    logging.info('home page visit at %s UTC [currently running on COMPUTERNAME
+        (datetime.utcnow(), getenv('COMPUTERNAME', '<i>unknown</i>')))
     text = { 'content': 'Welcome to this lovely looking flask application !' } 
     return render_template('home.html',
         title = 'Flasky Home',
         text = text)
-
-
 
 app.debug = True
 if __name__ == "__main__":
