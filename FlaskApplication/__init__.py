c86694ea8e4e55764f78f3984d7502753f8495cd
Bill Wilder <codingoutloud@gmail.com>
2014-07-10 13:17:27 -0400

    Update __init__.py

--- FlaskApplication/__init__.py
+++ FlaskApplication/__init__.py
@@ -37,7 +37,7 @@ def home():
         simple_counter, os.getenv('COMPUTERNAME', 'unknown'))
 
 
-    text = { 'content': 'This flask site has been visited [%d] times!!!!!!' % simple_counter } 
+    text = { 'content': 'This sophisticated flask application has been visited [%d] times!!!!!!' % simple_counter } 
 
 
     return render_template('home.html',
